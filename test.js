729314f43f46355e1b2e8e0562961f15a8b97e76
avinduhewa <avinduhewa@gmail.com>
2017-01-19 14:30:05 +0530

    mocha test environment specs added in package and eslint

--- test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-describe ('First Test', () => {
-  it ('should pass when git push is initiated', () => {
-    assert.equal(true, true);
-  });
-});
---
+++ test/test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+const assert = require('assert');
+
+describe('First Test', () => {
+  it('should pass when git push is initiated', () => {
+    assert.equal(true, true);
+  });
+});
