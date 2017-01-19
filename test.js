a8abc706316ed670c1f56eb69e64411188e2043f
avinduhewa <avinduhewa@gmail.com>
2017-01-19 13:12:59 +0530

    improve code quality

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var assert = require('assert');
+var assert = require("assert");
 
-describe('First Test',function() {
-    it('should pass when git push is initiated', function() {
+describe("First Test",function() {
+    it("should pass when git push is initiated", function() {
         assert.equal(true,true);
-    })
-})
+    });
+});
