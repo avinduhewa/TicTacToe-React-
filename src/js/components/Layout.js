1ac6f2e9fa9dfa05668839a4d19e154497ec1398
avinduhewa <avinduhewa@gmail.com>
2017-01-26 11:00:26 +0530

    set up react dev environment

---
+++ src/js/components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Sample Text</h1>
+      </div>
+    );
+  }
+}
