36ff017502f33d2e5a6d81b6d0985303743ecba9
avinduhewa <avinduhewa@gmail.com>
2017-01-26 13:46:19 +0530

    created dummy landing page and added dummy create game button

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,11 +1,22 @@
+/* eslint-disable */
 import React from 'react';
 
 export default class Layout extends React.Component {
 
   render() {
+    const background = {
+      height: '769px',
+      backgroundImage: 'url("./assets/img/wallpaper.png")',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    };
     return (
-      <div>
-        <h1>Sample Text</h1>
+      <div class="text-center" style={background}>
+        <div class="jumbotron text-center" style={{backgroundColor:'rgba(0,0,0,0)', color:'#4e5d6c'}}>
+          <h1>TicTacToe</h1>
+          <p>A simple TicTacToe game made using ReactJS</p>
+        </div>
+        <p><a class="btn btn-primary btn-lg">Create game</a></p>
       </div>
     );
   }
